f543b661ad7a5405235ca5720369877ee3a5591b
Heraldo <heraldo@hackerrank.com>
2015-02-10 13:30:51 -0800

    Test Mac Repo
    Just for learning purposes

---
+++ 14609.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+class A {
+        public:
+                int data;
+                virtual void fun(){}
+                virtual void fun1() {}
+                virtual void fun2() {}
+};
+
+int main(){
+
+        A a;
+        cout << sizeof(a) << endl;
+
+return 0;
+}
